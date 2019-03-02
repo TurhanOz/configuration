60ff3c3bfb5895d8fde6c883d1a2c139b06189f9
Turhan Oz <@turhanoz>
2019-03-02 14:48:25 +0100

    [server][docker]

--- server_post_configuration.sh
+++ server_post_configuration.sh
@@ -26,12 +26,26 @@ set_defaults () {
   update-alternatives --set editor /usr/bin/vim.basic
 }
 
+install_docker () {
+  apt install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  apt update
+  apt-cache policy docker-ce
+  apt install docker-ce --assume-yes
+  systemctl status docker
+}
 
+clean () {
+    apt autoremove
+}
 main () {
 	update_distribution
 	install_base_packages
 	install_speedtest
 	install_angular
+	install_docker
+	clean
 	set_defaults
 }
 
